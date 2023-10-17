cd3c3ea73bddd95541934e6cd238189a3bacef16
Eld0d0 <nils.gill.arion@gmail.com>
2023-10-17 15:22:53 +0200

    Commentaire (erreurs)
    Ajout de commentaire pour répertorier l'ensemble des bugs trouvé dans le code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+    /* is_run n'était pas un boléen alors que son utilisation lui demande de l'être */
     let is_run = true;
 
     init();
 
     function init() {
+        /* L'écouteur 'click' était mal écrit et il manquait l'accolade de la fonction */
         document.querySelector('.button').addEventListener('click', (event) => {
             is_run = !is_run;
+                /* Une faute de frappe sur un document */
                 ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class') + ' pause');
         })
-
+        /* La fonction setInterval était mal écrite */
         setInterval(function () {
             if (is_run) {
-                let oDate = new Date();
+                /* Non obligatoire : la date peut être une constante */
+                const oDate = new Date();
                 document.querySelector('#hours').innerHTML = adjustTimer(oDate.getHours());
                 document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
                 document.querySelector('#seconds').innerHTML = adjustTimer(oDate.getSeconds());
@@ -19,13 +23,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
 
             }
+            /* Il manquait la parenthèse de fermeture de la fonction setInterval */
         }, 1000);
     }
 
     function adjustTimer(timer) {
         return (timer < 10 ? '0' + timer : timer);
     }
-
+    /* Il manquait z */
     function randomHexColor(x, y, z) {
         return "rgb(" + Math.floor(x / 100 * 256) + "," + Math.floor(y / 100 * 256) + "," + Math.floor(z / 100 * 256) + ")";
     }
--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
+<!-- Non obligatoire mais l'attribut lang manquait -->
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
+    <!-- Il manquait la balise fermante title -->
     <title>Timetitle</title>
+    <!-- Le lien des Google n'était pas bon -->
     <link href="https://fonts.googleapis.com/css?family=Aldrich" rel="stylesheet">
+    <!-- Le dossier asset était mal renseigné -->
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="assets/js/script.js"></script>
 </head>
 
 <body>
     <div id="wrapper">
+        <!-- L'id wrapper était de trop -->
         <div class="inside">
             <div class="timer"><span id="hours">00</span><span id="minutes">00</span><span id="seconds">00</span></div>
             <div class="controls"><button class="button pause"></button></div>
         </div>
     </div>
-
+<!-- Il manquait le / pour fermer le body -->
 </body>
 
 </html>
