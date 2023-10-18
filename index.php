baabb22a410f39ee06f32ecdcf8dc2cc17fc2cc7
Damien Cuda <damien.cuda@gmail.com>
2023-10-18 15:14:46 +0200

    Formatage index

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<!DOCTYPE html><html><head><meta charset="utf-8"><title>Timetitle> <link href="https://fonts.googleapis.comm/css?family=Aldrich" rel="stylesheet"> <link rel="stylesheet" href="asset/css/style.css"><script src="assets/js/script.js"></script></head><body><div id="wrapper"><div class="inside" id="wrapper"><div class="timer"><span id="hours">00</span><span id="minutes">00</span><span id="seconds">00</span></div><div class="controls"><button class="button pause"></button></div></div></div><body></html>
+<!-- ETAPE DE DEBUG -->
+<!-- 1  Reformatage du code-->
+<!-- 2 -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <!--Balise Title non fermée -->
+        <title>Timetitle</title>
+        <link href="https://fonts.googleapis.comm/css?family=Aldrich" rel="stylesheet">
+        <!-- Coquille appel dossier css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div class="inside" id="wrapper">
+                <div class="timer">
+                    <span id="hours">00</span>
+                    <span id="minutes">00</span>
+                    <span id="seconds">00</span>
+                </div>
+                <div class="controls">
+                    <button class="button pause"></button>
+                </div>
+            </div>
+        </div>
+        <!-- Body mal fermé -->
+    </body>
+</html>
