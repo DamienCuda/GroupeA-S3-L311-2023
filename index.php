29af242817be2341a58e5d17bac73b8427339019
Damien Cuda <damien.cuda@gmail.com>
2023-10-18 15:43:34 +0200

    appel google font

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
         <meta charset="utf-8">
         <!--Balise Title non fermée -->
         <title>Timetitle</title>
-        <link href="https://fonts.googleapis.comm/css?family=Aldrich" rel="stylesheet">
+        <!-- Appel Google font erroné -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Aldrich&display=swap" rel="stylesheet"> 
         <!-- Coquille appel dossier css -->
         <link rel="stylesheet" href="assets/css/style.css">
         <script src="assets/js/script.js"></script>
