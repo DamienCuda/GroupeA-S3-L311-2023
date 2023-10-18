fe78c97d8bf02582f326c0dc462a98127807f2f0
Damien Cuda <damien.cuda@gmail.com>
2023-10-18 18:00:14 +0200

    commentaires scenario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  // true sous forme de string au lieu de bool
+  //ERREUR : true sous forme de string au lieu de bool
   let is_run = true;
 
   init();
 
-  // mauvaise indentation et clic au lieu de click
+  //ERREUR : Mauvaise indentation et clic au lieu de click et acollades manquantes
   function init() {
     document.querySelector(".button").addEventListener("click",(event) => {
         is_run = !is_run;
@@ -15,28 +15,35 @@ window.addEventListener("DOMContentLoaded", (event) => {
     );
   }
 
-  // Adaptation pour plus de lisibilité
+  //ERREUR : Adaptation pour plus de lisibilité
   setInterval(function () {
+    // Si la is run est a true l'horloge tourne
     if (is_run) {
       
+      // Avec un nouvel objet date on récupère et injecte dans les span respectif les heures, minutes et secondes.
       let oDate = new Date();
       document.querySelector("#hours").innerHTML = adjustTimer(oDate.getHours());
       document.querySelector("#minutes").innerHTML = adjustTimer(oDate.getMinutes());
       document.querySelector("#seconds").innerHTML = adjustTimer(oDate.getSeconds());
 
+
       hours = document.querySelector("#hours").innerHTML;
       minutes = document.querySelector("#minutes").innerHTML;
       seconds = document.querySelector("#seconds").innerHTML;
 
+      // On adapte la couleur du body en utilisant les heures, minutes et secondes qu'on passe à la fonction randomHexColor
       document.querySelector("body").style.backgroundColor = randomHexColor(hours, minutes,seconds);
     }
   }, 1000);
 
-  // Return manquant
+  // ERREUR : Return manquant
+  // La fontion ajoute un 0 devant le paramètre si celui ci est inférieur à 10
   function adjustTimer(timer) {
     return timer < 10 ? "0" + timer : timer;
   }
-  //Adaptation de la fonction pour plus de lisibilité 
+  // ERREUR : Mauvaise construction de la chaine de carctères. Adaptation de la fonction pour plus de lisibilité
+  // La fonction récupère trois variables et renvoie un string, construit sur le format rgb(x,y,z).
+  // Le calcul utilisé dans l'objet Math et sa propiété .floor sert à renvoyer des valeurs comprise entre 0 et 256
   function randomHexColor(x, y, z) {
     r = Math.floor((x / 100) * 256) 
     g = Math.floor((y / 100) * 256) 
