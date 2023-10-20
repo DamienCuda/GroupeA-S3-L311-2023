fd536415395286f4feb5d69a7d698e7bf1e0a284
mxrlxw <121444190+nsa-dev@users.noreply.github.com>
2023-10-20 17:20:15 +0200

    Fix script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,34 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    let is_run    = "true";
-
     init();
 
     function init(){
-       document.querySelector('.button').addEventListener('clic', (event) => 
-            is_run = !is_run
-            ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? dcument.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class')+' pause');
-        })
+       var is_run = true;
+       document.querySelector('.button').addEventListener('click', (event) => { // <-- Correctif: "clic" au lieu de "click"
+            // Correctif: les inversions du type !is_run ne fonctionnent pas
+            if(is_run) is_run = false;
+            else is_run = true;
+            ((' ' + document.querySelector('.button').className + ' ').indexOf('pause') > -1) ? document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class').replace(' pause', '')) : document.querySelector('.button').setAttribute('class', document.querySelector('.button').getAttribute('class')+' pause');
+        });
 
-        setTimeInterval(function(){
+        // Correctif: Anciennement setTimeInterval
+        setInterval(function(){
             if(is_run){
                 let oDate = new Date();
-                document.querySelector('#hours').innerHTML   =  adjustTimer(oDate.getHours());
-                document.querySelector('#minutes').innerHTML =  adjustTimer(oDate.getMinutes());
-                document.querySelector('#seconds').innerHTML =  adjustTimer( oDate.getSeconds());
+                document.querySelector('#hours').innerHTML = adjustTimer(oDate.getHours());
+                document.querySelector('#minutes').innerHTML = adjustTimer(oDate.getMinutes());
+                document.querySelector('#seconds').innerHTML = adjustTimer( oDate.getSeconds());
 
                 document.querySelector('body').style.background = randomHexColor(document.querySelector('#hours').innerHTML, document.querySelector('#minutes').innerHTML, document.querySelector('#seconds').innerHTML);
                 
             }
-        }, 1000;
+        }, 1000); // Correctif: la parenthèse du setInterval n'était jamais fermée
     }
 
     function adjustTimer(timer){
-        (timer < 10 ? '0'+timer : timer);
+        return (timer < 10 ? '0'+timer : timer);
     }
 
-    function randomHexColor(x, y){
+    function randomHexColor(x, y, z){
         return "rgb(" + Math.floor(x/100 * 256) + "," + Math.floor(y/100 * 256) + "," + Math.floor(z/100 * 256) + ")";
     }
 });
